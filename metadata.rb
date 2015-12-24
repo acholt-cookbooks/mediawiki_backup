fbdd30a2c4b915b32e66aa21d672e7d18120386a
Andrew Holt <andrew@theholts.org>
2015-12-24 09:15:46 +0200

    SuperMarket Support - Adding Supported OS's - Adding Source and Issue URL's

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,14 @@ maintainer_email 'andrew@theholts.org'
 license          'GNU Public License 3.0'
 description      'Installs/Configures mediawiki_backup'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.0'
+version          '0.3.1'
+
+%w{ centos redhat }.each do |osel|
+  supports osel, '>= 5.0'
+end
+supports 'ubuntu', '>= 12.04'
+
+source_url 'https://github.com/acholt/chef_mediawiki_backup'
+issues_url 'https://github.com/acholt/chef_mediawiki_backup/issues'
 
 depends 'cron', '~> 1.7.0'
